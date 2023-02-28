bf11cef995fcba51897f6c281eb24cb2a547c40f
ISSAM LAOUMRI <issamlaoumri@gmail.com>
2023-02-28 21:47:16 +0100

    Startup : Adding basic project.

---
+++ src/main/java/com/ensa/pfa/gestioncooperatives/GestionCooperativesApplication.java
@@ -0,0 +1,19 @@
+package com.ensa.pfa.gestioncooperatives;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCooperativesApplication implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCooperativesApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        System.out.println("Test Test...");
+    }
+}
---
+++ src/test/java/com/ensa/pfa/gestioncooperatives/GestionCooperativesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensa.pfa.gestioncooperatives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCooperativesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
